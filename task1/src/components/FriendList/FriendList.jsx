bc23d0851813d15ec6e210019c9df11d16c24bf9
Mykola Kushka <Mykola.Kushka@gmail.com>
2025-01-01 16:43:38 +0200

    task added

---
+++ task1/src/components/FriendList/FriendList.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import FriendListItem from "./FriendListItem";
+import styles from "./FriendList.module.css";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li key={id} className={styles.item}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
+
+export default FriendList;
